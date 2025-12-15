5ca20de9c5b2eb83631628227ecd7aaecc9de706
Lejoy <2753398396@qq.com>
2025-12-15 14:56:22 +0800

    增强ProtocolDataMapping链式写入与属性支持
    新增Write/Property链式写入方法，支持通过PropertyValueSetterBool<TProtocol>实现属性式链式赋值，提升批量写入的易用性和代码流畅性。同时将NuGet包版本号升级至2.1.1。

--- src/ZMotionSDK/IOSugar/ProtocolDataMapping.cs
+++ src/ZMotionSDK/IOSugar/ProtocolDataMapping.cs
@@ -1,17 +1,44 @@
 using MessageToolkit;
 using MessageToolkit.Abstractions;
 using MessageToolkit.Models;
+using System.Linq.Expressions;
 
 namespace ZMotionSDK;
 
 public class ProtocolDataMapping<TProtocol> : NativeDataMapping<TProtocol, bool>
     where TProtocol : struct
 {
+    private readonly NativeDataMapping<TProtocol, bool> _baseMapping;
     private readonly Action<IEnumerable<WriteFrame<bool>>> _commitAction;
 
-    internal ProtocolDataMapping(IProtocolSchema<TProtocol> Schema, Action<IEnumerable<WriteFrame<bool>>> commitAction) : base(Schema)
+    internal ProtocolDataMapping(IProtocolSchema<TProtocol> Schema, Action<IEnumerable<WriteFrame<bool>>> commitAction)
+        : base(Schema)
     {
+        _baseMapping = new NativeDataMapping<TProtocol, bool>(Schema);
         _commitAction = commitAction;
+    }
+
+    public ProtocolDataMapping<TProtocol> Write(Expression<Func<TProtocol, bool>> fieldSelector, bool value)
+    {
+        _baseMapping.Write(fieldSelector, value);
+        return this;
+    }
+
+    public ProtocolDataMapping<TProtocol> Write(ushort address, bool value)
+    {
+        _baseMapping.Write(address, value);
+        return this;
+    }
+
+    public PropertyValueSetterBool<TProtocol> Property(ushort address)
+    {
+        return new PropertyValueSetterBool<TProtocol>(this, address);
+    }
+
+    public PropertyValueSetterBool<TProtocol> Property(Expression<Func<TProtocol, bool>> fieldSelector)
+    {
+        ushort address = _schema.GetAddress(fieldSelector);
+        return new PropertyValueSetterBool<TProtocol>(this, address);
     }
 
     /// <summary>
@@ -21,5 +48,23 @@ public class ProtocolDataMapping<TProtocol> : NativeDataMapping<TProtocol, bool>
     {
         var frames = BuildOptimized();
         _commitAction(frames);
+    }
+}
+
+public sealed class PropertyValueSetterBool<TProtocol> where TProtocol : struct
+{
+    private readonly ProtocolDataMapping<TProtocol> _writeMapping;
+    private readonly ushort _address;
+
+    public PropertyValueSetterBool(ProtocolDataMapping<TProtocol> writeMapping, ushort address)
+    {
+        _writeMapping = writeMapping;
+        _address = address;
+    }
+
+    public ProtocolDataMapping<TProtocol> Value(bool value)
+    {
+        _writeMapping.Write(_address, value);
+        return _writeMapping;
     }
 }
